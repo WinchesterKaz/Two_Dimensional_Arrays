36a38da8ad706bd40c4934ac3681403a685a8193
Alena <alena.supernatural666@gmail.com>
2023-12-31 20:59:58 +0300

    Задача №3

--- Project_5/Program.cs
+++ Project_5/Program.cs
@@ -43,57 +43,127 @@
 //}
 //----------------------------------------------------------------------------------------------------------
 //Задача 2: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
-int[,] array = {
-          {4, 3, 1},
-          {2, 6, 9},
-          {4, 6, 2}
-       };
+//int[,] array = {
+          //{4, 3, 1},
+          //{2, 6, 9},
+          //{4, 6, 2}
+       //};
 
-      Console.WriteLine("Исходный массив:");
-       PrintArray(array);
+      //Console.WriteLine("Исходный массив:");
+       //PrintArray(array);
 
          //Меняем местами первую и последнюю строки
-        SwapFirstAndLastRow(array);
+        //SwapFirstAndLastRow(array);
 
-       Console.WriteLine("Массив после замены:");
-       PrintArray(array);
+       //Console.WriteLine("Массив после замены:");
+       //PrintArray(array);
     
 
-   static void SwapFirstAndLastRow(int[,] array)
-   {
-        int rows = array.GetLength(0);
-        int cols = array.GetLength(1);
+   //static void SwapFirstAndLastRow(int[,] array)
+   //{
+        //int rows = array.GetLength(0);
+        //int cols = array.GetLength(1);
 
        // Создаем временный массив
-        int[] temp = new int[cols];
+        //int[] temp = new int[cols];
         
          //Копируем первую строку во временный массив
-     for (int i = 0; i < cols; i++)
-       {
-          temp[i] = array[0, i];
-       }
+     //for (int i = 0; i < cols; i++)
+       //{
+          //temp[i] = array[0, i];
+       //}
 
         //Копируем последнюю строку в первую
-      for (int i = 0; i < cols; i++)
-       {
-           array[0, i] = array[rows - 1, i];
-      }
+      //for (int i = 0; i < cols; i++)
+       //{
+           //array[0, i] = array[rows - 1, i];
+      //}
 
         // Копируем временный массив (первую строку) в последнюю
-     for (int i = 0; i < cols; i++)
-     {
-         array[rows - 1, i] = temp[i];
-      }
-   }
+     //for (int i = 0; i < cols; i++)
+     //{
+         //array[rows - 1, i] = temp[i];
+      //}
+   //}
 
-   static void PrintArray(int[,] array)
-   {
-       for (int i = 0; i < array.GetLength(0); i++)
-       {
-           for (int j = 0; j < array.GetLength(1); j++)
-           {
-             Console.Write(array[i, j] + " ");
-         }
-          Console.WriteLine();
-      }
+   //static void PrintArray(int[,] array)
+   //{
+       //for (int i = 0; i < array.GetLength(0); i++)
+       //{
+           //for (int j = 0; j < array.GetLength(1); j++)
+           //{
+             //Console.Write(array[i, j] + " ");
+         //}
+          //Console.WriteLine();
+      //}
+   //}
+   //------------------------------------------------------------------------------------------------------
+   //Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку 
+//с наименьшей суммой элементов.
+int Prompt(string message)
+{
+Console.Write(message);
+ int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int[,] CreateArray(int m, int n)
+{
+ int[,] array = new int[m, n];
+  return array;
+}
+
+void PrintArray(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+  {
+   for (int j = 0; j < array.GetLength(1); j++)
+  {
+     Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] FillArrayRandom(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+  {
+   for (int j = 0; j < array.GetLength(1); j++)
+  {
+    array[i, j] = new Random().Next(1, 10);
    }
+  }
+ return array;
+}
+
+int SumRowArray(int[,] array)
+{
+ int[] sumArr = new int[array.GetLength(0)];
+  Console.WriteLine($"Сумма по строке:");
+ for (int i = 0; i < array.GetLength(0); i++)
+ {
+   for (int j = 0; j < array.GetLength(1); j++)
+   {
+     sumArr[i] += array[i, j];
+   }
+   Console.WriteLine($"{i + 1} -> {sumArr[i]}");
+  }
+ int minSum = sumArr[0];
+ int minRow = 1;
+ for (int i = 0; i < array.GetLength(0); i++)
+ {
+  if (sumArr[i] < minSum)
+   {
+     minSum = sumArr[i];
+      minRow = i + 1;
+   }
+ }
+  return minRow;
+}
+
+int[,] array = CreateArray(Prompt("Введите количество строк: "), Prompt("Введите количество столбцов: "));
+array = FillArrayRandom(array);
+PrintArray(array);
+int row = SumRowArray(array);
+Console.WriteLine($"Наименьшая сумма в {row} строке.");
